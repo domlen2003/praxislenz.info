13b4926ad441e3d9be885af03250dc362a4f2ee5
Dominik Lenz <dominikalexanderlenz@gmail.com>
2021-04-10 04:16:01 +0200

    -added sessions

--- handlers/admin_router.go
+++ handlers/admin_router.go
@@ -3,43 +3,55 @@ package handlers
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"html/template"
 	"net/http"
 )
 
 var tpl *template.Template
 
+var store = sessions.NewCookieStore([]byte("secret")) //TODO: replace with os.Getenv("SESSION_KEY")
+
 func AddAdminRouter(r *mux.Router, t *template.Template) {
 	tpl = t
 	r.HandleFunc("/", indexHandler)
 	r.HandleFunc("/login", loginHandler)
 	r.HandleFunc("/loginauth", loginAuthHandler).Methods("POST")
+	r.HandleFunc("/settings", settingsHandler)
+}
+
+func settingsHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+	fmt.Fprint(w, "Settings Page")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	/*	_, err := fmt.Fprintln(w, "Admin")
-		if err != nil {
-			log.Fatal("IndexHandler: ", err)
-		}*/
 	//redirect To Login Page
-
 	http.Redirect(w, r, r.URL.Path+"/login", http.StatusPermanentRedirect)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-
+	//Show Form for Login
 	tpl.ExecuteTemplate(w, "login.gohtml", nil)
 }
 
 func loginAuthHandler(w http.ResponseWriter, r *http.Request) {
+
 	r.ParseForm()
 	username := r.FormValue("username")
 	password := r.FormValue("password")
 	fmt.Println("Username: ", username, " Password: ", password)
 
-	if username != "Silvia Lenz" && password != "12345" {
+	if username != "Silvia Lenz" && password != "12345" { //TODO: replace with os.Getenv("PWD"), os.Getenv("USERNAME")
 		tpl.ExecuteTemplate(w, "login.gohtml", "Username or Password wrong!")
 	} else {
-		fmt.Fprint(w, "You´re logged in successfully.")
+		session, _ := store.Get(r, "session")
+		session.Values["username"] = username
+		session.Save(r, w)
+		http.Redirect(w, r, "/admin/settings", http.StatusPermanentRedirect)
 	}
 }
