7fce2df0ee824b9ad0f35c21c139782fb62e8465
Dominik Lenz <dominikalexanderlenz@gmail.com>
2021-04-11 17:57:43 +0200

    -removed tls because of cloudflare

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"crypto/tls"
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
-	"golang.org/x/crypto/acme/autocert"
 	"html/template"
 	"log"
 	"net/http"
@@ -28,42 +26,13 @@ func main() {
 	http.Handle("/", errorChain.Then(r))
 	http.Handle("/assets/", errorChain.Then(http.StripPrefix("/assets", http.FileServer(http.Dir("./templates/assets")))))
 
-	certManager := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		Cache:  autocert.DirCache("certs"),
-	}
-	server := &http.Server{
-		Addr:    ":443",
-		Handler: r,
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
-	}
-
-	go func() {
-		// serve HTTP, which will redirect automatically to HTTPS
-		h := certManager.HTTPHandler(nil)
-		log.Fatal(http.ListenAndServe(":http", h))
-	}()
-
 	// serve HTTPS!
-	log.Fatal(server.ListenAndServeTLS("", ""))
-	//err := http.ListenAndServe(":8080", nil)
-}
-
-type IndexContent struct {
-	Title   string
-	Message string
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Server: ", err)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, _ *http.Request) {
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
-	/*	tmpl := template.Must(template.ParseFiles("praxislenz.info/templates/index.gohtml"))
-		data := IndexContent{
-			Title: "Hello",
-		}
-		err := tmpl.Execute(w, data)
-		if err != nil {
-			log.Fatal("IndexHandler: ", err)
-		}*/
 }
